a01330de7668fa0f2fa9ca5ffa68ca7813c2a744
joshua.crews00@gmail.com <joshua.crews01@gmail.com>
2022-02-01 17:22:20 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+"""
+This is the main module for running TextRecognizer
+"""
+
+
+def print_hi(name):
+    """
+    Prints `Hi` and then a name provided
+    :param name: Insert a string to print the name
+    """
+    print(f'Hi, {name}')
+
+
+if __name__ == '__main__':
+    print_hi('Joshua')
